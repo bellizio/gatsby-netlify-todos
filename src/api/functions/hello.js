8866d34c47c283ca56f1f23a68e8858cb65bba5a
Devon Bellizio <devon.bellizio@passportinc.com>
2019-12-18 21:00:16 -0500

    502 error troublshooting continued

--- src/api/functions/hello.js
+++ src/api/functions/hello.js
@@ -1,9 +1,16 @@
-import db from '../db';
+// import db from '../db';
+import mongoose from 'mongoose';
+
+const dbUrl =
+  'mongodb+srv://dbellizio:Uk1SRtIb81sSDetW@cluster0-1tvyj.mongodb.net/test?retryWrites=true&w=majority';
+const dbOptions = {
+  useNewUrlParser: true,
+};
 
 // For more info, check https://www.netlify.com/docs/functions/#javascript-lambda-functions
 exports.handler = async (event, context, callback) => {
   try {
-    await db();
+    await mongoose.connect(dbUrl, dbOptions);
 
     return {
       statusCode: 200, // http status code
