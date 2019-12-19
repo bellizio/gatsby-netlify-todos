d74ff90f6b20ff32f1b15b4a4409543e32623145
Devon Bellizio <devon.bellizio@passportinc.com>
2019-12-18 20:37:29 -0500

    re-enable db connection to continue troubleshooting 502 error

--- src/api/functions/hello.js
+++ src/api/functions/hello.js
@@ -3,7 +3,7 @@ import db from '../db';
 // For more info, check https://www.netlify.com/docs/functions/#javascript-lambda-functions
 exports.handler = async (event, context, callback) => {
   try {
-    // await db();
+    await db();
 
     return {
       statusCode: 200, // http status code
