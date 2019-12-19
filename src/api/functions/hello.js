34c0caee7240e72f0c419c7ddc0b76c382898937
Devon Bellizio <devon.bellizio@passportinc.com>
2019-12-18 21:13:22 -0500

    revert changes re: troublshooting db connection

--- src/api/functions/hello.js
+++ src/api/functions/hello.js
@@ -1,16 +1,9 @@
-// import db from '../db';
-import mongoose from 'mongoose';
-
-const dbUrl =
-  'mongodb+srv://dbellizio:Uk1SRtIb81sSDetW@cluster0-1tvyj.mongodb.net/test?retryWrites=true&w=majority';
-const dbOptions = {
-  useNewUrlParser: true,
-};
+import db from '../db';
 
 // For more info, check https://www.netlify.com/docs/functions/#javascript-lambda-functions
 exports.handler = async (event, context, callback) => {
   try {
-    await mongoose.connect(dbUrl, dbOptions);
+    await db();
 
     return {
       statusCode: 200, // http status code
