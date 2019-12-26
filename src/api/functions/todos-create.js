227a7ff90b20b42f07bd3da017e14f01f9576ab2
Devon Bellizio <devon.bellizio@passportinc.com>
2019-12-26 10:05:50 -0600

    add crud functions for todos

--- src/api/functions/todos-create.js
+++ src/api/functions/todos-create.js
@@ -1,14 +1,13 @@
 import db from '../db';
 
-// For more info, check https://www.netlify.com/docs/functions/#javascript-lambda-functions
 exports.handler = async (event, context, callback) => {
   try {
     await db();
 
     return {
-      statusCode: 200, // http status code
+      statusCode: 200,
       body: JSON.stringify({
-        msg: 'Hello, World! ' + Math.round(Math.random() * 10),
+        msg: 'Todo created',
       }),
     };
   } catch (error) {
---
+++ src/api/functions/todos-delete.js
@@ -0,0 +1,16 @@
+import db from '../db';
+
+exports.handler = async (event, context, callback) => {
+  try {
+    await db();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        msg: 'Todo deleted',
+      }),
+    };
+  } catch (error) {
+    throw Error(error);
+  }
+};
