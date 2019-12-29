9da0c5c9a4f22cd04bc11003c3f78cd36df75398
Devon Bellizio <devon.bellizio@passportinc.com>
2019-12-29 09:28:06 -0500

    remove test api call

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import axios from 'axios';
 import Layout from '../components/layout';
 import Image from '../components/image';
 import SEO from '../components/seo';
 
 const IndexPage = () => {
-  axios.get('/.netlify/functions/todos-read').then(console.log);
-
   return (
     <Layout>
       <SEO title="Home" />
