782a922d114df7d05e748039d874d09d0a056934
Devon Bellizio <devon.bellizio@passportinc.com>
2019-12-29 10:48:18 -0500

    complete todos-delete function

--- src/api/functions/todos-delete.js
+++ src/api/functions/todos-delete.js
@@ -1,16 +1,31 @@
 import db from '../db';
+import TodoModel from '../models/todo.model';
+import { getId } from '../../utils';
 
 exports.handler = async (event, context, callback) => {
   try {
     await db();
+    const id = getId(event.path);
+    const deletedTodo = await TodoModel.findByIdAndRemove(id);
+
+    if (!deletedTodo) {
+      const error = new Error(`Todo with ID ${id} not found.`);
+      const formattedError = {
+        ...error,
+        statusCode: 404,
+        message: error.message,
+      };
+      throw formattedError;
+    }
 
     return {
       statusCode: 200,
-      body: JSON.stringify({
-        msg: 'Todo deleted',
-      }),
+      body: JSON.stringify(deletedTodo),
     };
-  } catch (error) {
-    throw Error(error);
+  } catch (e) {
+    return {
+      statusCode: e.statusCode || 500,
+      body: e.message || 'Internal server error.',
+    };
   }
 };
