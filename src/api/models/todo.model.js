c8c185728fa787242cf42767ff255c89f1b5e98c
Devon Bellizio <devon.bellizio@passportinc.com>
2020-03-25 15:43:38 -0400

    use timestamps option in todoSchema

--- src/api/models/todo.model.js
+++ src/api/models/todo.model.js
@@ -2,14 +2,20 @@ import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
-const todoSchema = new Schema({
-  name: String,
-  completed: {
-    type: Boolean,
-    default: false,
+const todoSchema = new Schema(
+  {
+    name: String,
+    completed: {
+      type: Boolean,
+      default: false,
+    },
   },
-  created_at: { type: Date, default: Date.now },
-  updated_at: { type: Date, default: Date.now },
-});
+  {
+    timestamps: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at',
+    },
+  }
+);
 
 export default mongoose.model('Todo', todoSchema);
