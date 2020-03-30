b0e2d91f979afa8c7f66d3ec22ad7fcd451580ff
Devon Bellizio <devon.bellizio@passportinc.com>
2020-03-29 21:26:40 -0400

    update header and layout to use material ui components

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,16 @@
-import { Link } from 'gatsby';
-import PropTypes from 'prop-types';
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-);
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-};
-
-Header.defaultProps = {
-  siteTitle: ``,
+const Header = () => {
+  return (
+    <AppBar position="sticky">
+      <Toolbar>
+        <Typography variant="h6">Todos</Typography>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Header;
