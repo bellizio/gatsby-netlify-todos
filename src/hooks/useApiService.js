f5beb61c46b4b390ab2745b433ebe04311302d57
Devon Bellizio <devon.bellizio@passportinc.com>
2020-03-29 20:45:04 -0400

    add useApiService hook

---
+++ src/hooks/useApiService.js
@@ -0,0 +1,61 @@
+import { useEffect, useReducer } from 'react';
+
+const FETCH_INIT = 'FETCH_INIT';
+const FETCH_SUCCESS = 'FETCH_SUCCESS';
+const FETCH_FAILURE = 'FETCH_FAILURE';
+
+const initalState = {
+  isError: false,
+  isLoading: false,
+  data: null,
+};
+
+const dataFetchReducer = (state, action) => {
+  const { type, data } = action;
+
+  switch (type) {
+    case FETCH_INIT:
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case FETCH_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data,
+      };
+    case FETCH_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+    default:
+      throw new Error();
+  }
+};
+
+const useApiService = (fetchMethod, params) => {
+  const [state, dispatch] = useReducer(dataFetchReducer, initalState);
+
+  const fetchData = async () => {
+    try {
+      dispatch({ type: FETCH_INIT });
+      const { data } = await fetchMethod(params);
+      dispatch({ type: FETCH_SUCCESS, data });
+    } catch (error) {
+      dispatch({ type: FETCH_FAILURE });
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return [state];
+};
+
+export default useApiService;
