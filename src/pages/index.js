534f5b689db1c6590749d1f49ab9f32e9e23a2e9
Devon Bellizio <devon.bellizio@passportinc.com>
2020-03-29 20:54:04 -0400

    add TodoList component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import TodoList from '../components/TodoList';
 
 const IndexPage = () => {
   return (
     <Layout>
       <SEO title="Home" />
+      <TodoList />
     </Layout>
   );
 };
