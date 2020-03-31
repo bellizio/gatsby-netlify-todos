dd2390282feb456c49125b403e0a995cf4baa40c
Devon Bellizio <devon.bellizio@passportinc.com>
2020-03-31 18:37:33 -0400

    add FUNCTIONS_BASE_PATH const

--- src/services/todo.service.js
+++ src/services/todo.service.js
@@ -1,22 +1,24 @@
 import axios from 'axios';
 
+const FUNCTIONS_BASE_PATH = '/.netlify/functions';
+
 export const createTodo = (data) => {
-  return axios.post(`/.netlify/functions/todos-create`, data);
+  return axios.post(`${FUNCTIONS_BASE_PATH}/todos-create`, data);
 };
 
 export const getTodo = (id) => {
-  return axios.get(`/.netlify/functions/todos-read/${id}`);
+  return axios.get(`${FUNCTIONS_BASE_PATH}/todos-read/${id}`);
 };
 
 export const getAllTodos = () => {
-  return axios.get(`/.netlify/functions/todos-read-all`);
+  return axios.get(`${FUNCTIONS_BASE_PATH}/todos-read-all`);
 };
 
 export const updateTodo = (data) => {
   const id = 0; // todo: parse id from data obj
-  return axios.put(`/.netlify/functions/todos-update/${id}`, data);
+  return axios.put(`${FUNCTIONS_BASE_PATH}/todos-update/${id}`, data);
 };
 
 export const removeTodo = (id) => {
-  return axios.delete(`/.netlify/functions/todos-delete/${id}`);
+  return axios.delete(`${FUNCTIONS_BASE_PATH}/todos-delete/${id}`);
 };
