23178d1390ac2a1eeb182736c50a52e8bdcc70ca
Devon Bellizio <devon.bellizio@passportinc.com>
2020-04-02 18:41:12 -0400

    get basic list working

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,8 +1,43 @@
 import React from 'react';
-import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Checkbox from '@material-ui/core/Checkbox';
+import useApiService from '../hooks/useApiService';
+import { getAllTodos } from '../services/todo.service';
 
 const TodoList = () => {
-  return <Box my={2}>Todo List</Box>;
+  const [state] = useApiService([], getAllTodos);
+  const { data, isLoading } = state;
+
+  if (isLoading && !data.length) {
+    return <p>Loading...</p>;
+  }
+
+  const listItems = data.map((todo, i) => {
+    const labelId = `checkbox-list-label-${i}`;
+
+    return (
+      <ListItem key={todo._id} dense button>
+        <ListItemIcon>
+          <Checkbox
+            edge="start"
+            // checked={checked.indexOf(todo) !== -1}
+            tabIndex={-1}
+            disableRipple
+            inputProps={{ 'aria-labelledby': labelId }}
+          />
+        </ListItemIcon>
+        <ListItemText id={labelId} primary={todo.name} />
+      </ListItem>
+    );
+  });
+
+  const list = <List>{listItems}</List>;
+
+  return <Container maxWidth="sm">{list}</Container>;
 };
 
 export default TodoList;
