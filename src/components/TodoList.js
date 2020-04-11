88cc1510dd66151b45cb226af0c35034916695c0
Devon Bellizio <devon.bellizio@passportinc.com>
2020-04-10 21:24:48 -0400

    wire up list item toggle handler

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from '@material-ui/core/Container';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -10,21 +10,35 @@ import { getAllTodos } from '../services/todo.service';
 
 const TodoList = () => {
   const [state] = useApiService([], getAllTodos);
+  const [checkedItems, setCheckedItems] = useState([]);
   const { data, isLoading } = state;
 
   if (isLoading && !data.length) {
     return <p>Loading...</p>;
   }
 
+  const handleListItemToggle = (item) => () => {
+    const currentIndex = checkedItems.indexOf(item);
+    const newChecked = [...checkedItems];
+
+    if (currentIndex === -1) {
+      newChecked.push(item);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setCheckedItems(newChecked);
+  };
+
   const listItems = data.map((todo, i) => {
     const labelId = `checkbox-list-label-${i}`;
 
     return (
-      <ListItem key={todo._id} dense button>
+      <ListItem key={todo._id} dense button onClick={handleListItemToggle(i)}>
         <ListItemIcon>
           <Checkbox
             edge="start"
-            // checked={checked.indexOf(todo) !== -1}
+            checked={checkedItems.indexOf(i) !== -1}
             tabIndex={-1}
             disableRipple
             inputProps={{ 'aria-labelledby': labelId }}
