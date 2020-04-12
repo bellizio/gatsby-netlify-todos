9461a1c2664de251df23fc69fc2216bea1b2df60
Devon Bellizio <devon.bellizio@passportinc.com>
2020-04-12 15:24:40 -0400

    remove ListItemSecondaryAction; remove onClick handler from ListItem; add onChange handler to Checkbox

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,9 +4,6 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import IconButton from '@material-ui/core/IconButton';
-import DeleteIcon from '@material-ui/icons/Delete';
 import Checkbox from '@material-ui/core/Checkbox';
 import Loading from './Loading';
 import useApiService from '../hooks/useApiService';
@@ -21,7 +18,7 @@ const TodoList = () => {
     return <Loading />;
   }
 
-  const handleListItemToggle = (item) => () => {
+  const handleCheckboxToggle = (item) => () => {
     const currentIndex = checkedItems.indexOf(item);
     const newChecked = [...checkedItems];
 
@@ -38,10 +35,11 @@ const TodoList = () => {
     const labelId = `checkbox-list-label-${i}`;
 
     return (
-      <ListItem key={todo._id} dense button onClick={handleListItemToggle(i)}>
+      <ListItem key={todo._id} dense button>
         <ListItemIcon>
           <Checkbox
             edge="start"
+            onChange={handleCheckboxToggle(i)}
             checked={checkedItems.indexOf(i) !== -1}
             tabIndex={-1}
             disableRipple
@@ -49,11 +47,6 @@ const TodoList = () => {
           />
         </ListItemIcon>
         <ListItemText id={labelId} primary={todo.name} />
-        <ListItemSecondaryAction>
-          <IconButton edge="end" aria-label="trash">
-            <DeleteIcon />
-          </IconButton>
-        </ListItemSecondaryAction>
       </ListItem>
     );
   });
