7bc61630c1895a6d4275b2465045ae37bb926164
Devon Bellizio <devon.bellizio@passportinc.com>
2020-04-18 12:23:02 -0400

    add error message to default case

--- src/hooks/useApiService.js
+++ src/hooks/useApiService.js
@@ -28,7 +28,7 @@ const dataFetchReducer = (state, action) => {
         isError: true,
       };
     default:
-      throw new Error();
+      throw new Error(`Unhandled action type: ${type}`);
   }
 };
 
