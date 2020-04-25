da98cd74972cd7e449f83d10b344e0631fb023ce
Devon Bellizio <devon.bellizio@passportinc.com>
2020-04-25 09:21:11 -0500

    update footer text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
 
 const Footer = () => (
   <footer>
-    © {new Date().getFullYear()}, Built with
-    {` `}
-    <a href="https://www.gatsbyjs.org">Gatsby</a>
+    <Typography variant="body1">
+      Built with{' '}
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.gatsbyjs.org"
+      >
+        Gatsby
+      </a>
+      ,{` `}
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.netlify.com/"
+      >
+        Netlify
+      </a>
+      , and {` `}
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.mongodb.com/cloud/atlas"
+      >
+        MongoDB
+      </a>
+    </Typography>
   </footer>
 );
 
