b127907a0f97d4a9f07be38810755bc28432c4df
Devon Bellizio <devon.bellizio@passportinc.com>
2020-04-26 09:17:29 -0500

    update siteMetadata; update app manifest data

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,9 +15,9 @@ module.exports = {
     );
   },
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Serverless Todos`,
+    description: `A simple serverless todos app built with Gatsby, Netlify, and MongoDB.`,
+    author: `@bellizio`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -34,13 +34,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `gatsby-netlify-todos`,
+        short_name: `todos`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
